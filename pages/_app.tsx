d3008f1e3d6822cfcf94b98277e894f672bd97c3
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-06-21 21:47:27 +0100

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import '@/styles/globals.scss'
+import type { AppProps } from 'next/app'
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
+}
